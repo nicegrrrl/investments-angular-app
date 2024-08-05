81755d82362cbe67dc3ef405303960d19b344c27
Alana Maia <alanaalvesmaia@gmail.com>
2024-08-05 09:01:39 -0300

    migrate all the componentes to modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,7 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {
-  // resultsData = signal<
-  //   | {
-  //       year: number;
-  //       interest: number;
-  //       valueEndOfYear: number;
-  //       annualInvestment: number;
-  //       totalInterest: number;
-  //       totalAmountInvested: number;
-  //     }[]
-  //   | undefined
-  // >(undefined);
-}
+export class AppComponent {}
